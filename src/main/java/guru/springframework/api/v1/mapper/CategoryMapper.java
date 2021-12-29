3c58074581e069a1baf2e68fa36021a05738ce2f
Phil Fernandez <philvfernandez@gmail.com>
2021-12-29 13:59:10 -0700

    Added MapStruct IntelliJ plugin and example mapping annotation for ID.

--- src/main/java/guru/springframework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/guru/springframework/api/v1/mapper/CategoryMapper.java
@@ -4,6 +4,7 @@ import guru.springframework.api.v1.model.CategoryDTO;
 import guru.springframework.domain.Category;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 
@@ -14,5 +15,6 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
